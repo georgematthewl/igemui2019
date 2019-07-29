8f0df1a6017dd01f6c2c226a44149445b8100ea8
George Matthew Limongan <matthew.limongan@gmail.com>
2019-07-29 15:50:17 +0700

    Add scroll up button and progress bar

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
+  $(".fa-arrow-circle-up").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#top-page").offset().top
+      },
+      800
+    );
+  });
+
+  function toggleArrowUp() {
+    if (
+      document.body.scrollTop > 600 ||
+      document.documentElement.scrollTop > 600
+    ) {
+      document.getElementById("arrow-up").style.display = "block";
+    } else {
+      document.getElementById("arrow-up").style.display = "none";
+    }
+  }
+
+  function updateProgressBar() {
+    let winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    let height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
+
   $(document).scroll(function() {
     if (screen.width > 768) {
       let scrollPos = $(document).scrollTop();
@@ -8,5 +38,7 @@ $(document).ready(function() {
         $("#nav").removeClass("bg-blue");
       }
     }
+    toggleArrowUp();
+    updateProgressBar();
   });
 });
